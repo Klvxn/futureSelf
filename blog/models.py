a2298a7f700bf95527c7307ec789f1a076c10e93
Kelvin <akpulukelvin@gmail.com>
2022-08-16 22:15:16 +0100

    added bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
-from django.contrib.auth.models import User
 
 
 # Create your models here.
